323f2b87bf8975c4ac515205fe95d7057ca0ceb8
Przemek Konieczny <przemyslawkoniecznyy@gmail.com>
2023-10-11 16:22:40 +0200

    new commit

--- app/loading.js
+++ app/loading.js
@@ -3,7 +3,7 @@ import React from 'react'
 
 
 
-import {CircularProgress} from "@nextui-org/react";
+import {CircularProgress} from "@nextui-org/progress";
 
 export default function Loading() {
   return (
